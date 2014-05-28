83c4f7728adc6009a3094723a9d75422e22c2a68
Kevin Lee <kevinleeone@gmail.com>
2014-05-28 18:56:43 +0800

    [master] Revert "[master] Revert "[master] WIP""
    This reverts commit 763b228aba56d3ab269101a32fe804dce4b51ded.

--- lab2/cbp2014/sim/predictor.cc
+++ lab2/cbp2014/sim/predictor.cc
@@ -1,92 +1,73 @@
 #include "predictor.h"
 
-
-#define PHT_CTR_MAX  3
+#define PHT_CTR_MAX 3
 #define PHT_CTR_INIT 2
+#define HIST_LEN 17
 
-#define HIST_LEN   17
-
-/////////////// STORAGE BUDGET JUSTIFICATION ////////////////
-// Total storage budget: 32KB + 17 bits
-// Total PHT counters: 2^17
-// Total PHT size = 2^17 * 2 bits/counter = 2^18 bits = 32KB
-// GHR size: 17 bits
-// Total Size = PHT size + GHR size
-/////////////////////////////////////////////////////////////
-
-
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-
-PREDICTOR::PREDICTOR(void){
-
-  historyLength    = HIST_LEN;
-  ghr              = 0;
-  numPhtEntries    = (1<< HIST_LEN);
-
-  pht = new UINT32[numPhtEntries];
-
-  for(UINT32 ii=0; ii< numPhtEntries; ii++){
-    pht[ii]=PHT_CTR_INIT;
-  }
-
+PREDICTOR::PREDICTOR() {
+    m = 14;
+    n = 13;
+    localHistory = new UINT32[1 << m]();
+    localHistoryCounter = new UINT32[1 << n];
+    for (int i = 0; i < (1 << n); ++i) {
+        localHistoryCounter[i] = 2;
+    }
+    globalHistory = new UINT32[1 << n];
+    for (int i = 0; i < (1 << n); ++i) {
+        globalHistory[i] = 2;
+    }
+    meta = new UINT32[1 << n];
+    for (int i = 0; i < (1 << n); ++i) {
+        meta[i] = 2;
+    }
 }
 
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-
-bool   PREDICTOR::GetPrediction(UINT32 PC){
-
-  UINT32 phtIndex   = (PC^ghr) % (numPhtEntries);
-  UINT32 phtCounter = pht[phtIndex];
-
-  if(phtCounter > PHT_CTR_MAX/2){
-    return TAKEN;
-  }else{
-    return NOT_TAKEN;
-  }
-
+PREDICTOR::~PREDICTOR() {
+    delete[] meta;
+    delete[] globalHistory;
+    delete[] localHistoryCounter;
+    delete[] localHistory;
 }
 
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
+bool PREDICTOR::GetPrediction(UINT32 PC) {
+    UINT32 phtIndex = (PC ^ ghr) % (numPhtEntries);
+    UINT32 phtCounter = pht[phtIndex];
+    if(phtCounter > PHT_CTR_MAX/2){
+        return TAKEN;
+    }else{
+        return NOT_TAKEN;
+    }
+}
 
 void  PREDICTOR::UpdatePredictor(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget){
 
-  UINT32 phtIndex   = (PC^ghr) % (numPhtEntries);
-  UINT32 phtCounter = pht[phtIndex];
+    UINT32 phtIndex   = (PC^ghr) % (numPhtEntries);
+    UINT32 phtCounter = pht[phtIndex];
 
-  // update the PHT
+    // update the PHT
 
-  if(resolveDir == TAKEN){
-    pht[phtIndex] = SatIncrement(phtCounter, PHT_CTR_MAX);
-  }else{
-    pht[phtIndex] = SatDecrement(phtCounter);
-  }
+    if(resolveDir == TAKEN){
+        pht[phtIndex] = SatIncrement(phtCounter, PHT_CTR_MAX);
+    }else{
+        pht[phtIndex] = SatDecrement(phtCounter);
+    }
 
-  // update the GHR
-  ghr = (ghr << 1);
+    // update the GHR
+    ghr = (ghr << 1);
 
-  if(resolveDir == TAKEN){
-    ghr++;
-  }
+    if(resolveDir == TAKEN){
+        ghr++;
+    }
 
 }
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
 
 void    PREDICTOR::TrackOtherInst(UINT32 PC, OpType opType, UINT32 branchTarget){
 
-  // This function is called for instructions which are not
-  // conditional branches, just in case someone decides to design
-  // a predictor that uses information from such instructions.
-  // We expect most contestants to leave this function untouched.
+    // This function is called for instructions which are not
+    // conditional branches, just in case someone decides to design
+    // a predictor that uses information from such instructions.
+    // We expect most contestants to leave this function untouched.
 
-  return;
+    return;
 }
 
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////

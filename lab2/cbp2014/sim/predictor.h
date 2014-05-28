763b228aba56d3ab269101a32fe804dce4b51ded
Kevin Lee <kevinleeone@gmail.com>
2014-05-28 18:50:01 +0800

    [master] Revert "[master] WIP"
    This reverts commit 73f9d52267545f7a2cd292a03b870288ee62aad7.

--- lab2/cbp2014/sim/predictor.h
+++ lab2/cbp2014/sim/predictor.h
@@ -4,21 +4,35 @@
 #include "utils.h"
 #include "tracer.h"
 
-class PREDICTOR {
-private:
-    UINT32 m;
-    UINT32 n;
-    UINT32* localHistory;
-    UINT32* localHistoryCounter;
-    UINT32* globalHistory;
-    UINT32* meta;
-public:
-    PREDICTOR();
-    ~PREDICTOR();
-    bool GetPrediction(UINT32 PC);
-    void UpdatePredictor(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget);
-    void TrackOtherInst(UINT32 PC, OpType opType, UINT32 branchTarget);
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+class PREDICTOR{
+
+  // The state is defined for Gshare, change for your design
+
+ private:
+  UINT32  ghr;           // global history register
+  UINT32  *pht;          // pattern history table
+  UINT32  historyLength; // history length
+  UINT32  numPhtEntries; // entries in pht 
+
+ public:
+
+  // The interface to the four functions below CAN NOT be changed
+
+  PREDICTOR(void);
+  bool    GetPrediction(UINT32 PC);  
+  void    UpdatePredictor(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget);
+  void    TrackOtherInst(UINT32 PC, OpType opType, UINT32 branchTarget);
+
+  // Contestants can define their own functions below
+
 };
 
+
+
+/***********************************************************/
 #endif
 
